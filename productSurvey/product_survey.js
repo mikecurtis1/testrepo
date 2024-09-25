35074a45b67ec354833431107470039076dab757
Mike Curtis <mikecurtis1@gmail.com>
2024-09-25 14:44:29 -0400

    test

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -7,6 +7,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experience = document.getElementById('experinceText').value;
 
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -15,6 +16,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperince').innerHTML = experience;
     document.getElementById('userInfo').style.display = 'block';
 
     alert('Thank you for your valuable feedback')
